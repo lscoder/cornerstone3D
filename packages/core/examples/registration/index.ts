f2d425236b7f62da9c199b0cf95b0e7d86a91e2c
Leonardo Campos <leonardoscampos@gmail.com>
2023-11-06 00:36:52 -0300

    feat(registration): trying to improve performance

--- packages/core/examples/registration/index.ts
+++ packages/core/examples/registration/index.ts
@@ -43,6 +43,8 @@ console.warn(
   'Click on index.ts to open source code for this example --------->'
 );
 
+const DOWNSAMPLE_VOLUME = false;
+
 const dataTypesMap = {
   Int8: IntTypes.Int8,
   UInt8: IntTypes.UInt8,
@@ -73,15 +75,29 @@ let webWorker = null;
 const volumesInfo = [
   {
     volumeId: `${volumeLoaderScheme}:CT_VOLUME_ID_1`,
-    wadoRsRoot: 'https://d33do7qe4w26qo.cloudfront.net/dicomweb',
-    StudyInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125095438.5',
-    SeriesInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125095449.8',
+
+    // Neptune
+    // wadoRsRoot: 'https://d33do7qe4w26qo.cloudfront.net/dicomweb',
+    // StudyInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125095438.5',
+    // SeriesInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125095449.8',
+
+    // Juno
+    wadoRsRoot: 'http://localhost/dicom-web',
+    StudyInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125112931.11',
+    SeriesInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125113028.6',
   },
   {
     volumeId: `${volumeLoaderScheme}:CT_VOLUME_ID_2`,
-    wadoRsRoot: 'https://d33do7qe4w26qo.cloudfront.net/dicomweb',
-    StudyInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125095258.1',
-    SeriesInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125095305.12',
+
+    // Neptune
+    // wadoRsRoot: 'https://d33do7qe4w26qo.cloudfront.net/dicomweb',
+    // StudyInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125095258.1',
+    // SeriesInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125095305.12',
+
+    // Juno
+    wadoRsRoot: 'http://localhost/dicom-web',
+    StudyInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125113417.1',
+    SeriesInstanceUID: '1.3.6.1.4.1.25403.345050719074.3824.20170125113420.1',
   },
 ];
 
@@ -347,6 +363,72 @@ async function loadAndCacheAllParameterMaps() {
     const transformName = transformNames[i];
     const { parameterMap } = await getElastixParameterMap(transformName);
 
+    parameterMap['AutomaticTransformInitialization'] = ['true'];
+
+    if (transformName === 'rigid') {
+      parameterMap['FixedInternalImagePixelType'] = ['float'];
+      parameterMap['MovingInternalImagePixelType'] = ['float'];
+      parameterMap['Registration'] = ['MultiResolutionRegistration'];
+      parameterMap['Interpolator'] = ['BSplineInterpolator'];
+      parameterMap['ResampleInterpolator'] = ['FinalBSplineInterpolator'];
+      parameterMap['Resampler'] = ['DefaultResampler'];
+      parameterMap['FixedImagePyramid'] = ['FixedSmoothingImagePyramid'];
+      parameterMap['MovingImagePyramid'] = ['MovingSmoothingImagePyramid'];
+      parameterMap['Optimizer'] = ['AdaptiveStochasticGradientDescent'];
+      parameterMap['Transform'] = ['EulerTransform'];
+      parameterMap['Metric'] = ['AdvancedMattesMutualInformation'];
+      parameterMap['AutomaticScalesEstimation'] = ['true'];
+      parameterMap['AutomaticTransformInitialization'] = ['true'];
+      parameterMap['HowToCombineTransforms'] = ['Compose'];
+      parameterMap['ITKTransformOutputFileNameExtension'] = ['h5'];
+      parameterMap['WriteITKCompositeTransform'] = ['true'];
+      parameterMap['NumberOfHistogramBins'] = ['64'];
+      parameterMap['ErodeMask'] = ['false'];
+      parameterMap['NumberOfResolutions'] = ['3'];
+      parameterMap['ImagePyramidSchedule'] = '8 8 8 4 4 4 2 2 2 1 1 1'.split(
+        ' '
+      );
+      parameterMap['MaximumNumberOfIterations'] = ['500'];
+      parameterMap['MaximumStepLength'] = ['1.0'];
+      parameterMap['RequiredRatioOfValidSamples'] = ['0.05'];
+      parameterMap['NumberOfSpatialSamples'] = ['2000'];
+      parameterMap['NewSamplesEveryIteration'] = ['true'];
+      parameterMap['ImageSampler'] = ['Random'];
+    } else if (transformName === 'affine') {
+      parameterMap['FixedInternalImagePixelType'] = ['float'];
+      parameterMap['MovingInternalImagePixelType'] = ['float'];
+      parameterMap['Registration'] = ['MultiResolutionRegistration'];
+      parameterMap['Interpolator'] = ['BSplineInterpolator'];
+      parameterMap['ResampleInterpolator'] = ['FinalBSplineInterpolator'];
+      parameterMap['Resampler'] = ['DefaultResampler'];
+      parameterMap['FixedImagePyramid'] = ['FixedSmoothingImagePyramid'];
+      parameterMap['MovingImagePyramid'] = ['MovingSmoothingImagePyramid'];
+      parameterMap['Optimizer'] = ['AdaptiveStochasticGradientDescent'];
+      parameterMap['Transform'] = ['AffineTransform'];
+      parameterMap['Metric'] = ['AdvancedNormalizedCorrelation'];
+      parameterMap['AutomaticScalesEstimation'] = ['true'];
+      parameterMap['AutomaticTransformInitialization'] = ['true'];
+      parameterMap['HowToCombineTransforms'] = ['Compose'];
+      parameterMap['ITKTransformOutputFileNameExtension'] = ['h5'];
+      parameterMap['WriteITKCompositeTransform'] = ['true'];
+      parameterMap['NumberOfHistogramBins'] = ['32'];
+      parameterMap['ErodeMask'] = ['false'];
+      parameterMap['NumberOfResolutions'] = ['4'];
+      parameterMap['ImagePyramidSchedule'] = [
+        '40 40 4 20 20 2 10 10 1 5 5 1'.split(' '),
+      ];
+      parameterMap['MaximumNumberOfIterations'] = ['1000'];
+      parameterMap['NumberOfSpatialSamples'] = ['3000'];
+      parameterMap['NewSamplesEveryIteration'] = ['true'];
+      parameterMap['ImageSampler'] = ['RandomCoordinate'];
+      parameterMap['BSplineInterpolationOrder'] = ['1'];
+      parameterMap['FinalBSplineInterpolationOrder'] = ['3'];
+      parameterMap['DefaultPixelValue'] = ['0'];
+      parameterMap['WriteResultImage'] = ['false'];
+      parameterMap['ResultImagePixelType'] = ['short'];
+      parameterMap['ResultImageFormat'] = ['mhd'];
+    }
+
     defaultParameterMaps[transformName] = parameterMap;
     console.log(`Default parameter map (${transformName}):`, parameterMap);
   }
@@ -387,6 +469,81 @@ function loadParameterMap(transformName: string) {
   });
 }
 
+function downsampleVolume(srcDimensions, srcScalarData) {
+  const newDimensions = [
+    Math.floor(srcDimensions[0] / 2),
+    Math.floor(srcDimensions[1] / 2),
+    srcDimensions[2], // Math.floor(srcDimensions[2] / 2),
+  ];
+  const [newWidth, newHeight, newDepth] = newDimensions;
+
+  // Multiplied by 2 because it's a Int16Array
+  const bufferSize = newWidth * newHeight * newDepth * 2;
+  const sharedArrayBuffer = new SharedArrayBuffer(bufferSize);
+  const newScalarData = new Int16Array(sharedArrayBuffer);
+
+  const srcVoxelsPerSlice = srcDimensions[0] * srcDimensions[1];
+  let srcPixelIndex = 0;
+  let srcVoxelFront00 = 0;
+  let srcVoxelFront01 = srcVoxelFront00 + 1;
+  let srcVoxelFront10 = srcVoxelFront00 + srcDimensions[0];
+  let srcVoxelFront11 = srcVoxelFront10 + 1;
+  // let srcVoxelBack00 = srcVoxelFront00 + srcVoxelsPerSlice;
+  // let srcVoxelBack01 = srcVoxelBack00 + 1;
+  // let srcVoxelBack10 = srcVoxelBack00 + srcDimensions[0];
+  // let srcVoxelBack11 = srcVoxelBack10 + 1;
+
+  let newVoxelIndex = 0;
+
+  for (let xNew = 0; xNew < newWidth; xNew++) {
+    for (let yNew = 0; yNew < newHeight; yNew++) {
+      for (let zNew = 0; zNew < newDepth; zNew++) {
+        // const xOriginal = 2 * xNew;
+        // const yOriginal = 2 * yNew;
+        // const zOriginal = 2 * zNew;
+
+        // Compute the average value of the 2x2x2 block in the original volume
+        const averageValue =
+          (srcScalarData[srcVoxelFront00] +
+            srcScalarData[srcVoxelFront01] +
+            srcScalarData[srcVoxelFront10] +
+            srcScalarData[srcVoxelFront11]) /
+          4;
+
+        // (srcScalarData[srcPixelIndex] +
+        //   srcScalarData[srcPixelIndex + 1] +
+        //   srcScalarData[srcPixelIndex + srcDimensions[0]] +
+        //   srcScalarData[srcPixelIndex + srcDimensions[0] + 1]) /
+        // 4;
+
+        // srcScalarData[srcVoxelBack00] +
+        // srcScalarData[srcVoxelBack01] +
+        // srcScalarData[srcVoxelBack10] +
+        // srcScalarData[srcVoxelBack11]
+
+        // Set the voxel in the new volume to the computed average value
+        newScalarData[newVoxelIndex] = averageValue;
+        newVoxelIndex++;
+
+        srcPixelIndex += 2;
+        srcVoxelFront00 += 2;
+        srcVoxelFront01 += 2;
+        srcVoxelFront10 += 2;
+        srcVoxelFront11 += 2;
+        // srcVoxelBack00 += 2;
+        // srcVoxelBack01 += 2;
+        // srcVoxelBack10 += 2;
+        // srcVoxelBack11 += 2;
+      }
+    }
+  }
+
+  return {
+    dimentions: newDimensions,
+    scalarData: newScalarData,
+  };
+}
+
 /**
  * Get the ITK Image from a given viewport
  * @param viewportId - Viewport Id
@@ -403,18 +560,40 @@ function getImageFromViewport(viewportId, imageName?: string): Image {
   const imageData = volumeActor.getMapper().getInputData();
   const pointData = imageData.getPointData();
   const scalars = pointData.getScalars();
-  const dimensions = imageData.getDimensions();
+  let dimensions = imageData.getDimensions();
   const origin = imageData.getOrigin();
-  const spacing = imageData.getSpacing();
+  let spacing = imageData.getSpacing();
   const directionArray = imageData.getDirection();
   const direction = new Float64Array(directionArray);
   const numComponents = pointData.getNumberOfComponents();
-  const dataType = scalars
-    .getDataType()
-    .replace(/^Ui/, 'UI')
-    .replace(/Array$/, '');
+  // const dataType = scalars.getDataType().replace(/^Ui/, 'UI').replace(/Array$/, '');
+  const dataType = 'Int16';
   const metadata: Metadata = undefined;
-  const scalarData = scalars.getData();
+  let scalarData = scalars.getData();
+
+  if (DOWNSAMPLE_VOLUME) {
+    regConsole.log(`Downsampling volume (${imageName})...`);
+
+    const startTime = Date.now();
+    const downsampledVolumeData = downsampleVolume(dimensions, scalarData);
+    const totalTime = Date.now() - startTime;
+    const oldDimensions = [...dimensions];
+
+    scalarData = downsampledVolumeData.scalarData;
+    dimensions = downsampledVolumeData.dimentions;
+
+    regConsole.log(`Volume (${imageName}) downsampled in ${totalTime} ms`);
+    regConsole.log(`old dimensions: [${oldDimensions.join(',')}]`);
+    regConsole.log(`new dimentions: [${dimensions.join(',')}]`);
+    regConsole.log(`old spacing: [${spacing.join(',')}]`);
+
+    spacing = spacing.map(
+      (value, i) => (value * oldDimensions[i]) / dimensions[i]
+    );
+
+    regConsole.log(`new spacing: ${spacing.join(',')}`);
+  }
+
   const imageType: ImageType = new ImageType(
     dimensions.length,
     dataTypesMap[dataType],
@@ -430,10 +609,14 @@ function getImageFromViewport(viewportId, imageName?: string): Image {
   image.direction = direction;
   image.size = dimensions;
   image.metadata = metadata;
+  // image.data = scalarData;
   image.data = scalarData;
 
   // image.data = new scalarData.constructor(scalarData.length);
   // image.data.set(scalarData, 0);
+
+  // image.data = new Int16Array(originalPixelData.length);
+  // image.data.set(originalPixelData, 0);
 
   return image;
 }
